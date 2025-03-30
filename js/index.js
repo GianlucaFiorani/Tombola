af06ac21758f7f2acc4653aa177112ca7ddf64a5
Gianluca Fiorani <gianluca.fiorani97@gmail.com>
2025-03-30 23:44:38 +0200

    add all files

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 const tabellone = [];
 const tombola = document.getElementById("tombola");
+const player = document.getElementById("player");
 const numGenration = document.createElement("button");
 const estractionDiv = document.getElementById("estration");
+const form = document.querySelector("form");
+const h1 = document.createElement("h1");
 numGenration.innerText = "ESTRAI";
-estractionDiv.appendChild(numGenration);
+h1.innerText = "TOMBOLA";
 
 const tombolaGeneration = function () {
-  for (let i = 0; i < 99; i++) {
+  for (let i = 0; i < 90; i++) {
     tabellone.push(i + 1);
     const box = document.createElement("div");
     box.classList.add("casella");
@@ -15,6 +18,58 @@ const tombolaGeneration = function () {
     box.appendChild(h3);
     tombola.appendChild(box);
   }
+};
+
+const card = function () {
+  const playerCard = [...tabellone];
+  const container = document.createElement("div");
+  for (let i = 0; i < 15; i++) {
+    const randNum = Math.ceil(Math.random() * (playerCard.length - 1));
+    const playNum = playerCard[randNum];
+    const box = document.createElement("div");
+    box.classList.add("casella", "player");
+    container.classList.add("container");
+    const h3 = document.createElement("h3");
+    h3.innerText = playNum;
+    playerCard.splice(randNum, 1);
+    box.appendChild(h3);
+    container.appendChild(box);
+  }
+  player.appendChild(container);
+};
+
+const howMany = function (num) {
+  for (let i = 0; i < num; i++) {
+    card();
+  }
+};
+
+const win = function () {
+  const container = document.querySelectorAll(".container");
+  let count = 0;
+  container.forEach((box) => {
+    const check = box.querySelectorAll(".selected");
+    if (check.length === 15) {
+      const input = document.getElementById("input");
+      numGenration.style.display = "none";
+      input.appendChild(h1);
+    }
+  });
+};
+
+h1.onclick = function (e) {
+  window.location.reload();
+};
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  const numCard = document.getElementById("cardNum");
+  console.log(numCard.value);
+  const num = parseInt(numCard.value);
+  howMany(num);
+  numCard.style.display = "none";
+  const input = document.getElementById("input");
+  input.appendChild(numGenration);
 };
 
 numGenration.onclick = function (e) {
@@ -29,22 +84,16 @@ numGenration.onclick = function (e) {
       num.classList.add("selected");
     }
   });
+  win();
 };
 
-const player = document.getElementById("player");
-const card = function () {
-  for (let i = 0; i < 27; i++) {
-    const randNum = Math.ceil(Math.random() * 99);
-    const box = document.createElement("div");
-    box.classList.add("casella");
-    const h3 = document.createElement("h3");
-    h3.innerText = randNum;
-    box.appendChild(h3);
-    player.appendChild(box);
-  }
-};
+setInterval(() => {
+  h1.style.color = "rgb(255, 230, 156)";
+}, 200);
+setInterval(() => {
+  h1.style.color = "red";
+}, 400);
 
 window.addEventListener("DOMContentLoaded", function () {
   tombolaGeneration();
-  card();
 });
